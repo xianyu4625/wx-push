16c4422ca6f9f6cc63675a7bfea757ff89595a1f
xianyu4625 <106040617+xianyu4625@users.noreply.github.com>
2022-12-15 11:43:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
+  appid: "wxae03f95ef8899a30",
+  appsecret: "dc42c9da0041669a4c457b80466838cd",
   template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  key: "oHaWp6B3UkGk2BE9QiUdj5DGVz5M",
+  city: "青海",
+  first_date: "2022-01-10",
 };
